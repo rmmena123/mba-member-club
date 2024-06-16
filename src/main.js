7d68e61120789d5261c6fe12f6c1ed27866836b5
Rodrigo Mena <rodrigo.mena8632@gmail.com>
2024-06-16 00:25:47 -0300

    Config: using JSON Server and Webpack

---
+++ src/main.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// CSS
+import "./styles/global.css";
+import "./styles/header.css";
+import "./styles/history.css";
+import "./styles/loyal-card.css";
+import "./styles/loyal-progress.css";
+import "./styles/user-info.css";
+
+console.log("Olá mundo!");
